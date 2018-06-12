146f49392eefe7b82b64de0f9f8e9201ecb04ec1
ljh199207 <865531656@qq.com>
2018-06-12 22:03:51 +0800

    1

---
+++ src/main/java/com/data/dataDatail/test/MyLinkedListTest.java
@@ -0,0 +1,22 @@
+package com.data.dataDatail.test;
+
+import com.data.dataDatail.array.MyLinkedList;
+
+public class MyLinkedListTest {
+
+    public static void main(String[] args) {
+        MyLinkedList my = new MyLinkedList();
+        my.add(1);
+        my.add(2);
+        my.add(3);
+        my.add(4);
+        my.add(5);
+        my.add(6);
+        my.add(7);
+        my.add(8);
+        my.add(9);
+        my.add(10);
+
+    }
+
+}
